98718757ab896c11e16088f7aed87f01bb44e013
RobsonMaia <robsonmvieira@gmail.com>
2017-12-07 16:47:24 -0200

    publish design done

--- src/Domain/Entitties/Book.js
+++ src/Domain/Entitties/Book.js
@@ -37,7 +37,7 @@ const bookSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'publisher',
   }],
-  quantityHands:{
+  quantityHands: {
     type: Number,
     required: true,
   },
---
+++ src/Domain/Entitties/publish.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const publishSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  authors: [{
+    type: Schema.Types.ObjectId,
+    ref: 'author',
+  }],
+  books: [{
+    type: Schema.Types.ObjectId,
+    ref: 'book',
+  }],
+});
+
+const publish = mongoose.model('publish', publishSchema);
+
+module.exports = publish;
